57291a8c37c70e38d68f8c43df20d9b84197b1f9
Preston Miller <mill6528@morris.umn.edu>
2015-01-26 14:48:56 -0600

    Added tests for different cases of switch in different GPAs.

--- public/javascript/clientJavascript.js
+++ public/javascript/clientJavascript.js
@@ -44,20 +44,32 @@ var getLetter = function(i){
     }
 }
 
-calc = function(course1,credits1,course2,credits2,course3,credits3){
-    return (letterToNumber(course1)*credits1+letterToNumber(course2)*credits2+letterToNumber(course3)*credits3)/(credits1+credits2+credits3);
+var calc = function(course1,credits1,course2,credits2,course3,credits3){
+    return (Number(letterToNumber(course1))*Number(credits1)+Number(letterToNumber(course2))*Number(credits2)+Number(letterToNumber(course3))*Number(credits3))/(Number(credits1)+Number(credits2)+Number(credits3));
 }
 
-letterToNumber = function(course){
-    switch(course){
+var letterToNumber = function(course) {
+    switch (course.toUpperCase()) {
         case "A":
             return 4;
+        case "A-":
+            return 3.7;
         case "B":
             return 3;
+        case "B+":
+            return 3.3;
+        case"B-":
+            return 2.7;
         case "C":
             return 2;
+        case "C+":
+            return 2.3;
+        case "C-":
+            return 1.7;
         case "D":
             return 1;
+        case "D+":
+            return 1.3;
         default:
             return 0;
     }
--- public/javascript/clientJavascript.spec.js
+++ public/javascript/clientJavascript.spec.js
@@ -10,9 +10,9 @@ describe('testing basic functions', function(){
     });
 
     //this test should fail.
-    it('this test should fail!', function(){
-        expect(returnKittens()).toBe("Not Kittens!");
-    });
+//    it('this test should fail!', function(){
+//        expect(returnKittens()).toBe("Not Kittens!");
+//    });
 
     it('this test should Pass!', function(){
         expect(returnTrue()).toBe(true);
@@ -22,9 +22,78 @@ describe('testing basic functions', function(){
         expect(letterToNumber("D")).toBe(1);
    });
 
+    it("should be 1.3",function() {
+        expect(letterToNumber("D+")).toBe(1.3);
+    });
+
+    it("should be 1.7",function() {
+        expect(letterToNumber("C-")).toBe(1.7);
+    });
+
+    it("should be 2",function() {
+        expect(letterToNumber("C")).toBe(2);
+    });
+
+    it("should be 2.3",function() {
+        expect(letterToNumber("C+")).toBe(2.3);
+    });
+
+    it("should be 2.7",function() {
+        expect(letterToNumber("B-")).toBe(2.7);
+    });
+
+    it("should be 3",function() {
+        expect(letterToNumber("B")).toBe(3);
+    });
+
+    it("should be 3.3",function() {
+        expect(letterToNumber("B+")).toBe(3.3);
+    });
+
+    it("should be 3.7",function() {
+        expect(letterToNumber("A-")).toBe(3.7);
+    });
+
     it("should be 4",function() {
         expect(calc("A",5,"A",4,"A",1234)).toBe(4.0);
     });
+
+    it("should be 3",function() {
+        expect(calc("B",5,"B",4,"B",1234)).toBe(3.0);
+    });
+
+    it("should be 2",function() {
+        expect(calc("C",5,"C",4,"C",1234)).toBe(2.0);
+    });
+
+    it("should be 1",function() {
+        expect(calc("D",5,"D",4,"D",1234)).toBe(1.0);
+    });
+
+    it("should be 1.3",function() {
+        expect(calc("D+",5,"D+",4,"D+",1234)).toBe(1.3);
+    });
+
+    it("should be 1.7",function() {
+        expect(calc("C-",5,"C-",4,"C-",1234)).toBe(1.7);
+    });
+
+    it("should be 2.3",function() {
+        expect(calc("C+",5,"C+",4,"C+",1234)).toBe(2.3);
+    });
+
+    it("should be 2.7",function() {
+        expect(calc("B-",5,"B-",4,"B-",1234)).toBe(2.7);
+    });
+
+    it("should be 3.3",function() {
+        expect(calc("B+",5,"B+",4,"B+",1234)).toBe(3.3);
+    });
+
+    it("should be 3.7",function() {
+        expect(calc("A-",5,"A-",4,"A-",1234)).toBe(3.7);
+    });
+
 });
 
 
